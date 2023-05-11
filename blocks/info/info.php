cadad87655913a6803e058a73b2f36b70946fb3d
Aleksandar DevSpark <aleksandarkanjevac@icloud.com>
2023-05-11 11:42:12 +0200

    Create Info block

---
+++ blocks/info/info.php
@@ -0,0 +1,10 @@
+<?php
+$class = 'il_block il_section il_info_block';
+?>
+
+<div class="<?php echo $class; ?>">
+    <div class="container">
+        <?php get_template_part('components/images'); ?>
+        <?php get_template_part('components/intro'); ?>
+    </div>
+</div>
