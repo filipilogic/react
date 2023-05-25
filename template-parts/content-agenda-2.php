5d4c54be68f6f554c9f9cdc07a408adcdc03555c
Aleksandar DevSpark <aleksandarkanjevac@icloud.com>
2023-05-25 08:24:16 +0200

    Agenda style 2 remake

--- template-parts/content-agenda-2.php
+++ template-parts/content-agenda-2.php
@@ -9,15 +9,14 @@ $speakers_position = get_sub_field('speakers_position');
 ?>
 
 <div class="il_agenda_content">
-    <div class="il_ac_speakers_image">
-    <?php echo wp_get_attachment_image($speakers_image,'full') ?>
+    <div class="il_ac_speakers_left">
+        <p class="il_ac_content_time"><?php echo $agenda_time; ?></p>
     </div>
     <div class="il_ac_content">
         <div class="agenda-timeslot-container">
-        <p class="il_ac_content_time"><?php echo $agenda_time; ?></p>
             <p class="il_ac_content_duration"><?php echo $agenda_duration; ?></p>
+            <p class="il_ac_content_title"><?php echo $agenda_title; ?></p>
         </div>
-        <p class="il_ac_content_title"><?php echo $agenda_title; ?></p>
         <div class="il_ac_content_content"><?php echo $agenda_content; ?></div>
         <p class="il_ac_content_name"><?php echo $speakers_name; ?></p>
         <p class="il_ac_content_position"><?php echo $speakers_position; ?></p>
