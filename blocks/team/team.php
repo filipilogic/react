ba4b72983949144f8757fb07514b890177f71f8b
Aleksandar DevSpark <aleksandarkanjevac@icloud.com>
2023-06-02 11:08:52 +0200

    Create Advisory block

--- blocks/team/team.php
+++ blocks/team/team.php
@@ -61,14 +61,25 @@ if ( ! empty( $block['className'] ) ) {
 								<?php echo wp_get_attachment_image( $image, $size ); ?>
 							</figure>
 						<?php } ?>
-						<h4 class="member_name"><svg xmlns="http://www.w3.org/2000/svg" width="26.43" height="26.43" viewBox="0 0 26.43 26.43"><path id="Path_1429" data-name="Path 1429" d="M26.43,10.694v5.042a.687.687,0,0,1-.687.687H16.423v9.319a.687.687,0,0,1-.687.687H10.694a.688.688,0,0,1-.687-.687V16.423H.687A.688.688,0,0,1,0,15.736V10.694a.687.687,0,0,1,.687-.687h9.319V.687A.688.688,0,0,1,10.694,0h5.042a.687.687,0,0,1,.687.687v9.319h9.319A.687.687,0,0,1,26.43,10.694Z" fill="#009688"/></svg><?php echo $name; ?></h4>
+						<h4 class="member_name"><?php echo $name; ?></h4>
 						<span class="member_position"><?php echo $position ?></span>
+						<span class="member_button">Learn More</span>
 					</div>
 					<div class="member_text member_text_<?php echo $item; ?>">
+					<span class="close"><svg id="x" xmlns="http://www.w3.org/2000/svg" width="17.659" height="17.659" viewBox="0 0 17.659 17.659"><g id="Group_195" data-name="Group 195"><path id="Path_319" data-name="Path 319" d="M10.3,8.834l7.056-7.056A1.039,1.039,0,0,0,15.886.309L8.83,7.365,1.773.309A1.039,1.039,0,0,0,.3,1.777L7.361,8.834.3,15.89a1.039,1.039,0,1,0,1.469,1.469L8.83,10.3l7.056,7.056a1.039,1.039,0,0,0,1.469-1.469Z" transform="translate(0 -0.004)" fill="#2BB99B"/></g></svg></span>
 						<div class="member_text_inner">
-						<span class="close"><svg id="x" xmlns="http://www.w3.org/2000/svg" width="17.659" height="17.659" viewBox="0 0 17.659 17.659"><g id="Group_195" data-name="Group 195"><path id="Path_319" data-name="Path 319" d="M10.3,8.834l7.056-7.056A1.039,1.039,0,0,0,15.886.309L8.83,7.365,1.773.309A1.039,1.039,0,0,0,.3,1.777L7.361,8.834.3,15.89a1.039,1.039,0,1,0,1.469,1.469L8.83,10.3l7.056,7.056a1.039,1.039,0,0,0,1.469-1.469Z" transform="translate(0 -0.004)" fill="#fff"/></g></svg></span>
-							<h2 class="member_box_name tg_title_1 tg_light"><?php echo $name; ?></h2>
-							<div class="member_description"><?php echo $description; ?></div>
+							<div class="member_text_inner_left">
+								<?php if( $image ) { ?>
+									<figure class="member_image">
+										<?php echo wp_get_attachment_image( $image, $size ); ?>
+									</figure>
+								<?php } ?>
+							</div>
+							<div class="member_text_inner_right">
+								<h2 class="member_box_name tg_title_1 tg_light"><?php echo $name; ?></h2>
+								<span class="member_box_position"><?php echo $position ?></span>
+								<div class="member_box_description"><?php echo $description; ?></div>
+							</div>
 						</div>
 					</div>
 					<?php $item++;?>
